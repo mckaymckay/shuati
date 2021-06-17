9de55d4a264c30980b7c62e6549796bd33b7a757
mckaymckay <mckayjiang@163.com>
2021-06-17 16:25:00 +0800

    0617

--- 160.相交链表.py
+++ 160.相交链表.py
@@ -13,6 +13,17 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pa=headA
+        pb=headB
+        if pa and pb:
+            while pa !=pb:
+                pa=pa.next if pa else headB
+                pb=pb.next if pb else headA
+            return pa
+        else:
+            return None
+
+        
         
 # @lc code=end
 
--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-for i in range(0):
-    print(i)
+class ListNode:
+    def __init__(self,value=0):
+        self.value=value
+        self.next=None
+    
+class myList:
+    def __init__(self):
+        self.head=None
+
+a=ListNode(0)
+print(a.next)
